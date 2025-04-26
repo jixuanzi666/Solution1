e64ddc0a1757195f5af8bce7a0bbd21f77889fa1
jixuanzi <jixuanzi@tongji.edu.cn>
2025-04-27 02:38:36 +0800

    3

--- 111.cs
+++ 111.cs
@@ -5,10 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter the first number:");
-        int num1 = int.Parse(Console.ReadLine());
+        int num1;
+        while (!int.TryParse(Console.ReadLine(), out num1))
+        {
+        int num2;
+        while (!int.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number:");
+        }
+        }
 
         Console.WriteLine("Enter the second number:");
-        int num2 = int.Parse(Console.ReadLine());
+        int num2;
+        while (!int.TryParse(Console.ReadLine(), out num2))
+        {
+        Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+        }
 
         int sum = AddNumbers(num1, num2);
         Console.WriteLine($"The sum is: {sum}");
